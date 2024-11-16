30cc432cf9981a84867c929d5e2d01ada9063bd9
phiona-b3 <phionadev@gmail.com>
2024-11-16 10:18:31 +0300

    Setting Up Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black text-light-500">
-      Welcome to the world of Next.js
-    </h1>
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of Next.js
+      </h1>
+    </>
   );
 };
 
