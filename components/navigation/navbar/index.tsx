3eb4eb197e7898ba780564331559a996cd5bda14
phiona-b3 <phionadev@gmail.com>
2024-11-23 20:07:41 +0300

    Creating a Theme Toggler

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import Theme from "./Theme";
+
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
@@ -17,7 +19,9 @@ const Navbar = () => {
         </p>
       </Link>
       <p>Global Search</p>
-      <div className="flex-between gap-5">Theme</div>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
     </nav>
   );
 };
